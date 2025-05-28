67b867daab02b904275900b3e38600a08ab4cb4b
叶力靖 <yelijing90@gmail.com>
2025-05-28 23:40:29 +0800

    小实验

---
+++ shuxin/src/components/HelloButton.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h2>{{ message }}</h2>
+        <button @click="changeMessage">点我改文字</button>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const message = ref('你好，Vue！')
+
+function changeMessage() {
+    message.value = '你点击了按钮 🎉'
+}
+</script>
+
+<style scoped>
+button {
+    padding: 8px 16px;
+    font-size: 16px;
+}
+</style>
