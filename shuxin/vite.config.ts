cafd8db25f7da5d72ec6f9e529fa1ba07fbd2572
叶力靖 <yelijing90@gmail.com>
2025-05-29 20:22:10 +0800

    build: deploy to GitHub Pages

--- shuxin/vite.config.ts
+++ shuxin/vite.config.ts
@@ -1,12 +1,13 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
+  // 加上 base 路径，重要！
+  base: '/shuxin/',
+
   plugins: [
     vue(),
     vueJsx(),
